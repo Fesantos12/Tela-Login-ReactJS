fe0a7419a7ecf328eefeeb01208d1a6a1dfd1758
Fellipe <fellipeunker12@gmail.com>
2022-04-22 21:48:04 -0300

    login enviando dados pro console log pronto

--- tela-login/src/components/Login.jsx
+++ tela-login/src/components/Login.jsx
@@ -1,14 +1,62 @@
-import React from 'react' 
+import React from "react";
 
-import './Login.css'
-import { useState } from "react";
+import "./Login.css";
+import { useState } from 'react'
+
 
 const Login = () => {
+    
+    function cadastrarUsuario(e) {
+      e.preventDefault()
+      console.log(`Usuario: ${email} cadastrado com a senha ${password}`)
+    }
+    
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <>
-            
+          <div className="LoginContainer">
+            <h1>Login</h1>
+            <form action="">
+              <div className="EmailSenha">
+                <input
+                  type="Email"
+                  name="Email"
+                  id="Email"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  
+                />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Senha"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="RememberForgot">
+                <a href="#">Esqueceu sua senha?</a>
+              </div>
+              <div className="ButtonFlex">
+                <input
+                  type="button"
+                  value="Login"
+                  className="Button"
+                  onClick={cadastrarUsuario}
+                />
+              </div>
+              <div className="CriarConta">
+                <p>Não possui uma conta?</p>
+                <a href="#">
+                  <button>Criar Conta</button>
+                </a>
+              </div>
+            </form>
+          </div>
         </>
-    )
-}
+      );
+};
 
 export default Login;
